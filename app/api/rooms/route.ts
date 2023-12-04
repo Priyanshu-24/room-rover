573b95a8a2d3cc4e005f8425369aad70304b5d1c
priyanshu <priyanshu24pandey@gmail.com>
2023-12-04 16:21:17 +0530

    get and post room api implemented

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -1,5 +1,6 @@
+import { allRooms, newRoom } from "@/backend/controllers/roomControllers";
+
 import { NextRequest } from "next/server";
-import { allRooms } from "@/backend/controllers/roomControllers";
 import { createEdgeRouter } from "next-connect";
 import dbConnect from "@/config/dbConnect";
 
@@ -14,7 +15,12 @@ const router = createEdgeRouter<NextRequest, RequestContext>();
 dbConnect();
 
 router.get(allRooms);
+router.post(newRoom);
 
 export async function GET(request: NextRequest, ctx: RequestContext) {
+  return router.run(request, ctx);
+}
+
+export async function POST(request: NextRequest, ctx: RequestContext) {
   return router.run(request, ctx);
 }
--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,7 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import Room from "../models/room";
+
 export const allRooms = async (req: NextRequest) => {
+  const rooms = await Room.find();
+
+  const resPerPage: number = 8;
+
   return NextResponse.json({
-    data: "hello",
+    success: true,
+    resPerPage,
+    rooms,
+  });
+};
+
+export const newRoom = async (req: NextRequest) => {
+  const body = await req.json();
+
+  const room = await Room.create(body);
+
+  return NextResponse.json({
+    success: true,
+    room,
   });
 };
