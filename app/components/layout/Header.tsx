f05ee27d56815b3c9b12728c85f405ae63a0184b
priyanshu <priyanshu24pandey@gmail.com>
2023-12-15 14:34:11 +0530

    added logo in header

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex justify-between py-5 px-20 shadow-lg">
-      <Link href="/" className="font-semibold text-lg">
+    <header className="flex justify-between items-center py-5 px-20 shadow-lg">
+      <Link href="/" className="font-semibold text-lg flex items-center gap-1">
+        <Image
+          src="/images/logo.png"
+          width={30}
+          height={50}
+          alt="room-rover-logo"
+          quality={100}
+        />
         Room Rover
       </Link>
       <div>DropDown</div>
