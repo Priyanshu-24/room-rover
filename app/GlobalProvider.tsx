6852952ae6e7d7acd4155615ae6401a932f3b9a6
priyanshu <priyanshu24pandey@gmail.com>
2023-12-27 15:39:02 +0530

    added redux to the app

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
+import { store } from "@/redux/store";
 
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <Toaster />
-      <SessionProvider>{children}</SessionProvider>
+      <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+      </Provider>
     </NextUIProvider>
   );
 }
